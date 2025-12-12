54c6a7dda00e31a61d7036111b74ad50c0f4151a
mvonline007 <marc.verzotti@gmail.com>
2025-12-12 19:28:40 +0100

    Update dependencies for Python 3.12

--- smartmoneyconcepts/__init__.py
+++ smartmoneyconcepts/__init__.py
@@ -1,5 +1,9 @@
 import os
 from smartmoneyconcepts.smc import smc
 
-if os.getenv('SMC_CREDIT', '1') == '1':
-    print("\033[1;33mThank you for using SmartMoneyConcepts! ⭐ Please show your support by giving a star on the GitHub repository: \033[4;34mhttps://github.com/joshyattridge/smart-money-concepts\033[0m")
+__version__ = "0.0.27"
+
+if os.getenv("SMC_CREDIT", "1") == "1":
+    print(
+        "\033[1;33mThank you for using SmartMoneyConcepts! ⭐ Please show your support by giving a star on the GitHub repository: \033[4;34mhttps://github.com/joshyattridge/smart-money-concepts\033[0m"
+    )
